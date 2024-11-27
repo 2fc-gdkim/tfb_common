ec687eb60d0d343adacefd1b5aba5b7483b05e9c
2fc-gdkim <kdkim@2fc.co.kr>
2024-11-27 13:01:59 +0900

    로그 설정 함수 수정

--- src/utils/tracing_log.rs
+++ src/utils/tracing_log.rs
@@ -3,7 +3,7 @@ use tracing::Level;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer};
 use tracing_appender::rolling;
 
-pub fn setup(prefix: &str, log_path: &str, max_level: Level, file : bool) -> tracing_appender::non_blocking::WorkerGuard {
+pub fn setup(prefix: &str, log_path: &str, max_level: Level, with_console : bool) -> tracing_appender::non_blocking::WorkerGuard {
 
     let timer = time::format_description::parse(
         "[year]-[month padding:zero]-[day padding:zero] [hour]:[minute]:[second]",
@@ -47,16 +47,16 @@ pub fn setup(prefix: &str, log_path: &str, max_level: Level, file : bool) -> tra
         .with_filter(tracing_subscriber::filter::LevelFilter::from_level(max_level));
 
     // 두 개의 레이어를 병합하여 글로벌 설정
-    if file {
-        tracing_subscriber::registry()
-            .with(file_layer)
-            .init();
-    }
-    else {
+    if with_console {
         tracing_subscriber::registry()
         .with(file_layer)
         .with(console_layer)
         .init();        
+    }
+    else {
+        tracing_subscriber::registry()
+            .with(file_layer)
+            .init();
     }
 
     return _guard;
